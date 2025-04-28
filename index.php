8e515e1acc63e200952fc4dbf1bd114ae78113bc
yniverz <88168412+yniverz@users.noreply.github.com>
2025-04-28 09:11:02 +0200

    Add GitHub link to footer

--- index.php
+++ index.php
@@ -451,6 +451,7 @@ require __DIR__ . '/app/logic.php';    // Main "edit" / "view" mode logic
 
         <footer>
             <a id="change_password">Change Password</a>
+            <p>See this Project on <a href="https://github.com/yniverz/php-uni-support">GitHub</a></p>
             <p>Created by <a href="https://github.com/yniverz">yniverz</a></p>
             <p><?php echo htmlspecialchars(file_get_contents(__DIR__ . '/VERSION')); ?></p>
 
